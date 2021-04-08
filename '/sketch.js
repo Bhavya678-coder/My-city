19e20d84542686dced75dae0de7917a6720d88e5
Bhavya678-coder <82038413+Bhavya678-coder@users.noreply.github.com>
2021-04-08 19:52:36 +0800

    Add files via upload

---
+++ '/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400, 400);
+
+}
+
+function draw() {
+  background(0);
+  //draw building 1=20floors
+  rect(1,200,30,200);
+  //draw building 2=50floors
+  rect(50,150,30,250);
+  //draw building 3=10floors
+  rect(99,250,30,150);
+  //draw garden
+  rect(150,390,100,60);
+  //draw building 4=50floors
+  rect(270,150,30,250);
+  //draw building 5=15floos
+  rect(319,200,30,249)
+  //draw building 6=15floors
+  rect(358,200,30,249)
+}
+
